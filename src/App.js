0528449c138b6fb9300c3b05197b1440879ecb12
Lewis Jacobs <lewis.jacobs@redbite.com>
2022-02-01 15:21:53 +0000

    Added share

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
   const [guess, setGuess] = useState("");
   const [correct, setCorrect] = useState("");
   const [guesses, setGuesses] = useState([]);
+  const [canReveal, setCanReveal] = useState(true);
+  const [total, setTotal] = useState(0);
 
   return (
     <div className="App">
@@ -23,29 +25,48 @@ function App() {
         <div className='grid'>
           <img src="https://flagcdn.com/w2560/gb-eng.png" className='flag'/>
           <div className='panels'>
-            <div className={`first-panel ${firstInvis ? "invisible" : ""}`} onClick={() => setFirstInvis(true)}/>
-            <div className={`second-panel ${secondInvis ? "invisible" : ""}`} onClick={() => setSecondInvis(true)}/>
-            <div className={`third-panel ${thirdInvis ? "invisible" : ""}`} onClick={() => setThirdInvis(true)}/>
-            <div className={`fourth-panel ${fourthInvis ? "invisible" : ""}`} onClick={() => setFourthInvis(true)}/>
-            <div className={`fifth-panel ${fifthInvis ? "invisible" : ""}`} onClick={() => setFifthInvis(true)}/>
-            <div className={`sixth-panel ${sixthInvis ? "invisible" : ""}`} onClick={() => setSixthInvis(true)}/>
-            <div className={`seventh-panel ${seventhInvis ? "invisible" : ""}`} onClick={() => setSeventhInvis(true)}/>
-            <div className={`eighth-panel ${eighthInvis ? "invisible" : ""}`} onClick={() => setEightInvis(true)}/>
-            <div className={`ninth-panel ${ninthInvis ? "invisible" : ""}`} onClick={() => setNinthInvis(true)}/>
+            <div className={`first-panel ${firstInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setFirstInvis(true); setCanReveal(false); }}/>
+            <div className={`second-panel ${secondInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setSecondInvis(true); setCanReveal(false); }}/>
+            <div className={`third-panel ${thirdInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setThirdInvis(true); setCanReveal(false); }}/>
+            <div className={`fourth-panel ${fourthInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setFourthInvis(true); setCanReveal(false); }}/>
+            <div className={`fifth-panel ${fifthInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setFifthInvis(true); setCanReveal(false); }}/>
+            <div className={`sixth-panel ${sixthInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setSixthInvis(true); setCanReveal(false); }}/>
+            <div className={`seventh-panel ${seventhInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setSeventhInvis(true); setCanReveal(false); }}/>
+            <div className={`eighth-panel ${eighthInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setEightInvis(true); setCanReveal(false); }}/>
+            <div className={`ninth-panel ${ninthInvis ? "invisible" : ""}`} onClick={() => { if(canReveal) setNinthInvis(true); setCanReveal(false); }}/>
           </div>
         </div>
         <input value={guess} type="text" className="country" disabled={correct} onChange={(e) => setGuess(e.target.value)}/>
-        <button className='submit' disabled={correct || guess === ""} onClick={() => {
+        <button className='submit' onClick={() => {
 
-          if(guess.toLowerCase() === "england") setCorrect(true)
+          if(correct) {
+            navigator.clipboard.writeText("GeoGrid 1 " + total + "/9 " + "🟥".repeat(total) + "🟩".repeat(9-total));
+          } 
+          else {
 
-          let newGuesses = guesses;
-          newGuesses.push(guess);
-        
-          setGuesses(newGuesses)
-          setGuess("")
+            if(guess.toLowerCase() === "england") {
+              setTotal(firstInvis + secondInvis + thirdInvis + fourthInvis + fifthInvis + sixthInvis + seventhInvis + eighthInvis + ninthInvis);
+              setFirstInvis(true);
+              setSecondInvis(true);
+              setThirdInvis(true);
+              setFourthInvis(true);
+              setFifthInvis(true);
+              setSixthInvis(true);
+              setSeventhInvis(true);
+              setEightInvis(true);
+              setNinthInvis(true);
+              setCorrect(true);
+            }
+
+            let newGuesses = guesses;
+            newGuesses.push(guess);
+          
+            setGuesses(newGuesses)
+            setGuess("")
+            setCanReveal(true)
+          }
         }}>
-          Submit
+          {correct ? "Share" : "Submit"}
         </button>
         <div className='guesses'>
         {
