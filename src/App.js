e41e99cdcb85ae3e5e5d96591471324f5d0bb89f
Lewis Jacobs <lewis.jacobs@redbite.com>
2022-02-01 12:05:42 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [firstInvis, setFirstInvis] = useState(false);
+  const [secondInvis, setSecondInvis] = useState(false);
+  const [thirdInvis, setThirdInvis] = useState(false);
+  const [fourthInvis, setFourthInvis] = useState(false);
+  const [fifthInvis, setFifthInvis] = useState(false);
+  const [sixthInvis, setSixthInvis] = useState(false);
+  const [seventhInvis, setSeventhInvis] = useState(false);
+  const [eighthInvis, setEightInvis] = useState(false);
+  const [ninthInvis, setNinthInvis] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://flagcdn.com/w2560/gb-eng.png" className='flag'/>
+        <div className='grid'>
+          <div className={`first-panel ${firstInvis ? "invisible" : ""}`} onClick={() => setFirstInvis(true)}/>
+          <div className={`second-panel ${secondInvis ? "invisible" : ""}`} onClick={() => setSecondInvis(true)}/>
+          <div className={`third-panel ${thirdInvis ? "invisible" : ""}`} onClick={() => setThirdInvis(true)}/>
+          <div className={`fourth-panel ${fourthInvis ? "invisible" : ""}`} onClick={() => setFourthInvis(true)}/>
+          <div className={`fifth-panel ${fifthInvis ? "invisible" : ""}`} onClick={() => setFifthInvis(true)}/>
+          <div className={`sixth-panel ${sixthInvis ? "invisible" : ""}`} onClick={() => setSixthInvis(true)}/>
+          <div className={`seventh-panel ${seventhInvis ? "invisible" : ""}`} onClick={() => setSeventhInvis(true)}/>
+          <div className={`eighth-panel ${eighthInvis ? "invisible" : ""}`} onClick={() => setEightInvis(true)}/>
+          <div className={`ninth-panel ${ninthInvis ? "invisible" : ""}`} onClick={() => setNinthInvis(true)}/>
+        </div>
+        <input type="text" className='country'/>
       </header>
     </div>
   );
