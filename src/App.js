02d89588e84f35116ba2e95bebc8a7a5c0b37976
Lewis Jacobs <lewis.jacobs@redbite.com>
2022-02-01 15:36:14 +0000

    Changed to tick mark

--- src/App.js
+++ src/App.js
@@ -37,10 +37,10 @@ function App() {
           </div>
         </div>
         <input value={guess} type="text" className="country" disabled={correct || canReveal} onChange={(e) => setGuess(e.target.value)}/>
-        <button className='submit' onClick={() => {
+        <button className='submit' disabled={canReveal} onClick={() => {
 
           if(correct) {
-            navigator.clipboard.writeText("GeoGrid 1 " + total + "/9 " + "🟥".repeat(total) + "🟩".repeat(9-total));
+            navigator.clipboard.writeText("GeoGrid 1 " + total + "/9 " + "🟥".repeat(total) + "✅".repeat(9-total));
           } 
           else {
 
