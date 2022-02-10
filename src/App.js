a22eff7c5d89dfc62e13409f14e599d55df6de19
Lewis Jacobs <lewis.jacobs@redbite.com>
2022-02-10 23:10:05 +0000

    New flags

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
 import GeoTile from './pages/GeoTile';
-import { Route, Routes } from "react-router-dom";
-import WhosThatPokemon from './pages/WhosThatPokemon';
+// import { Route, Routes } from "react-router-dom";
+// import WhosThatPokemon from './pages/WhosThatPokemon';
 
 function App() {
 
   return (
-    <Routes >
-      <Route path="/" element={<GeoTile/>}/>
-      <Route path="/pokemon" element={<WhosThatPokemon/>}/>
-    </Routes > 
+    // <Routes >
+    //   <Route path="/" element={<GeoTile/>}/>
+    //   <Route path="/pokemon" element={<WhosThatPokemon/>}/>
+    // </Routes > 
+    <GeoTile/>
   );
 }
 
