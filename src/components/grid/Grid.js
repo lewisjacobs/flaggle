97cd99c90fa3e032c413f7d20aac59ab0d7bdb3a
Lewis Jacobs <lewis.jacobs@redbite.com>
2022-02-16 17:00:03 +0000

    Removing US state flags

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -38,15 +38,10 @@ function Grid({ title, description, rightAnswer, image, dayNumber, gameUrl, easy
 
   const max = easy ? 4 : 9;
 
-  const shareText =
-      `${title} #${dayNumber} ${total}/${max}\r\n` +
-      `${ correct ? "❌".repeat(total-1) + "✅" : "❌".repeat(total) }${"⬛".repeat(max-total)}\r\n` +
-      `${gameUrl}`;
-
   const handleGuess = () => {
 
     if(correct || gameOver) {
-      navigator.clipboard.writeText(shareText);
+      handleSharing();
     } else if(!canReveal) {
 
       if(guess === rightAnswer) {
@@ -72,10 +67,34 @@ function Grid({ title, description, rightAnswer, image, dayNumber, gameUrl, easy
 
       console.log(newGuesses.length)
 
-      if(newGuesses.length == max && !correct) setGameOver(true);
+      if(newGuesses.length === max && !correct) setGameOver(true);
       else setCanReveal(true);
     }
   }
+
+  const handleSharing = async () => {
+
+    const canonical = document.querySelector("link[rel=canonical]");
+    let url = canonical ? canonical.href : document.location.href;
+    const shareDetails = { url, title: `${title} #${dayNumber} ${total}/${max}`, text: `${ correct ? "❌".repeat(total-1) + "✅" : "❌".repeat(total) }${"⬛".repeat(max-total)}` };
+
+    if (navigator.share) {
+      try {
+        await navigator
+          .share(shareDetails)
+          .then(() =>
+            console.log("Hooray! Your content was shared to tha world")
+          );
+      } catch (error) {
+        console.log(`Oops! I couldn't share to the world because: ${error}`);
+      }
+    } else {
+      // fallback code
+      console.log(
+        "Web share is currently not supported on this browser. Please provide a callback"
+      );
+    }
+  };
 
   return (
       <div className='game'>
@@ -119,8 +138,8 @@ function Grid({ title, description, rightAnswer, image, dayNumber, gameUrl, easy
         <div className='guesses'>
         {
           guesses.map((g, index) => 
-            <div className={`guess-wrapper ${(index % 2 == 0) ? "filled" : ""}`} key={g}>
-              <div className={`guess-icon ${(index % 2 == 0) ? "filled" : ""}`}>{g === rightAnswer ? "✅" : "❌"}</div>
+            <div className={`guess-wrapper ${(index % 2 === 0) ? "filled" : ""}`} key={g}>
+              <div className={`guess-icon ${(index % 2 === 0) ? "filled" : ""}`}>{g === rightAnswer ? "✅" : "❌"}</div>
               <div className="guess-text">{g}</div>
             </div>
           )
